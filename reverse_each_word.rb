a084a54a927a20d86980decc2f68face86e29317
allison cortez <allizetorc@gmail.com>
2019-10-15 20:53:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,23 +1,6 @@
 
-
-# def reverse_each_word(sentence)
-#   #Since our sentence is a string, we'll need to turn it into an array, in order to reverse each word in place, not the entire string. We can do this by splitting the string.
-  
-#   split_string = sentence.split(' ')
-  
-#   #Now we'll store this new split string into an array
-#   reversed_array = []
-  
-#   #we need to know the size of our sentence
-#   sentence.collect {|i|  }
-#   reversed_array << split_string.pop 
-  
-# return reversed_array.join(' ')
-# end
-
 def reverse_each_word(string)
  string = string.split(' ')
- string_length = self.length - 1
  reversed_string = []
  
  string.collect do |word|
